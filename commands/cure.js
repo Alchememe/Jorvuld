0413864bcf03ca20e3e75d1119a315a4cff66c8e
Alchememe <74570040+Alchememe@users.noreply.github.com>
2020-11-25 21:06:45 +0000

    Created cure.js
    This is partially a test

---
+++ commands/cure.js
@@ -0,0 +1,8 @@
+module.exports= {
+    name: 'cure',
+    description: "Jorvuld will be cured of the plauge",
+    execute(message, args){
+        message.channel.send("By the Eight! Truly? With these I am saved. I'll have Lodiss deliver a message to a friend of mine who specializes in diseases.
+You've not only saved Tamriel from this horrid plague, but myself as well. I can't express how thankful I am.");
+    }
+}
