2e7cca0c119119ef322b3e4b49bf6ce0c7ad4ee2
Alchememe <74570040+Alchememe@users.noreply.github.com>
2020-11-25 21:29:59 +0000

    Added image

--- commands/cure.js
+++ commands/cure.js
@@ -2,6 +2,6 @@ module.exports= {
     name: 'cure',
     description: "Jorvuld will be cured of the plauge",
     execute(message, args){
-        message.channel.send("By the Eight! Truly? With these I am saved. I'll have Lodiss deliver a message to a friend of mine who specializes in diseases. You've not only saved Tamriel from this horrid plague, but myself as well. I can't express how thankful I am.");
+        message.channel.send("By the Eight! Truly? With these I am saved. I'll have Lodiss deliver a message to a friend of mine who specializes in diseases. You've not only saved Tamriel from this horrid plague, but myself as well. I can't express how thankful I am.", {files: ["https://i.imgur.com/NKQBw4r.png"]});
     }
 }
